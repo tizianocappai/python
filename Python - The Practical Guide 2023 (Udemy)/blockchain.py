c7184b4173542c92f08f0db9d20647caf6083784
tiziano.cappai <tiziano.cappai@accenture.com>
2023-01-22 17:07:43 +0100

    create blockchain file, using functions to create a 'blockchain'

---
+++ Python - The Practical Guide 2023 (Udemy)/blockchain.py
@@ -0,0 +1,16 @@
+blockchain = []
+
+
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
+
+
+add_value(1, [1])
+add_value(2, get_last_blockchain_value())
+add_value(3, get_last_blockchain_value())
+
+print(blockchain)

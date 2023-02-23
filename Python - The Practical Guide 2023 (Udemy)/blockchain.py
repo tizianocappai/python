1ac03ef5d5d63363aa08636c940ecedfbdb151ca
tiziano.cappai <tiziano.cappai@accenture.com>
2023-02-23 16:34:51 +0100

    add condition to while loop

--- Python - The Practical Guide 2023 (Udemy)/blockchain.py
+++ Python - The Practical Guide 2023 (Udemy)/blockchain.py
@@ -27,27 +27,40 @@ def print_blockchain():
     for block in blockchain:
         print('\nOutputting Block')
         print(block)
+    else:
+        print('-' * 20)
 
 
 def verify_chain():
-    block_index = 0
+    # block_index = 0
     is_valid = True
-    for block in blockchain:
+
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue
-        elif block[0] == blockchain[block_index - 1]:
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else:
             is_valid = False
-            break
 
-        block_index += 1
+        # for block in blockchain:
+        #     if block_index == 0:
+        #         block_index += 1
+        #         continue
+        #     elif block[0] == blockchain[block_index - 1]:
+        #         is_valid = True
+        #     else:
+        #         is_valid = False
+        #         break
+
+        #     block_index += 1
 
     return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print('\nPlease choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
@@ -65,12 +78,14 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Input was invaliid, please pick a value from the list!')
 
     if not verify_chain():
         print('Invalid blockchain!')
         break
+else:
+    print('User left!')
 
 print('\n\nDone!')

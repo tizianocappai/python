d310447db3cb999f1d009fc2f7fd787dce80bf18
tiziano.cappai <tiziano.cappai@accenture.com>
2023-02-23 15:12:51 +0100

    verify chain

--- Python - The Practical Guide 2023 (Udemy)/blockchain.py
+++ Python - The Practical Guide 2023 (Udemy)/blockchain.py
@@ -3,10 +3,14 @@ blockchain = []
 
 def get_last_blockchain_value():
     # arr[-1] return last element of an array
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -25,26 +29,48 @@ def print_blockchain():
         print(block)
 
 
-tx_amount = get_transaction_value()
-add_value(tx_amount)
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+
+        block_index += 1
+
+    return is_valid
+
 
 while True:
     print('\nPlease choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
     print('q: Quit')
 
     user_choice = get_user_choice()
 
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
+        add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == 'q':
         break
     else:
         print('Input was invaliid, please pick a value from the list!')
 
+    if not verify_chain():
+        print('Invalid blockchain!')
+        break
 
 print('\n\nDone!')
